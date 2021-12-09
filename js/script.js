bf8ee22fa8f546dea5a3380998f0c89fe0a7f8bc
Igor_T <igortronin@gmail.com>
2021-12-09 16:09:31 +0300

    Events

--- js/script.js
+++ js/script.js
@@ -24,3 +24,81 @@ const movieDB = {
     ]
 };
 
+//1
+
+const adv = document.querySelectorAll('.promo__adv img').forEach((item) => {
+    item.remove();
+});
+
+//2
+
+const genre = document.querySelector('.promo__genre');
+genre.textContent = 'Драма';
+
+//3
+
+const backGround = document.querySelector('.promo__bg');
+backGround.style.backgroundImage = 'url(../img/bg.jpg)';
+
+//4  //5
+
+/* movieDB.movies.sort();
+const films = document.querySelectorAll('.promo__interactive-item');
+films.forEach((item, i) => {
+   // films[i].textContent = (i + 1) + " " + movieDB.movies[i];                   //можно так!
+    films[i].textContent = `${i + 1} ${movieDB.movies[i]}`;                       //но лучше так!!!
+});
+ */
+
+const movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = '';
+movieDB.movies.sort();
+
+movieDB.movies.forEach((item, index) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${index + 1} ${item}
+        <div class="delete"></div>
+    </li>    
+    `;
+});
+
+
+
+//......................CСОБЫТИЯ И ОБРАБОТЧИКИ..........................//
+
+const btn = document.querySelector('button');       //событик клик
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.target.style.backgroundColor = 'yellow';
+    e.target.style.color = 'black';
+    console.log('click');
+});
+
+
+btn.addEventListener('mouseenter', (e) => {          //событиe при наведении мыши
+    e.preventDefault();
+    e.target.style.backgroundColor = 'red';
+    console.log('hover');
+});
+
+const logo = document.querySelector('.header__logo').addEventListener('click', (e) => {          //удаление элемента со страницы при клике
+    e.target.remove();
+});
+
+/*  const deleted = () => {
+    e.target.remove();
+}                                                                                             //или сначало создаем функцию и передаем ее аргументом
+const logos = document.querySelector('.header__logo');
+logos.addEventListener('click', deleted);
+
+const logoCont = document.querySelector('.header__logo');                                     //удаление обработчика событий
+logoCont.removeEventListener('click', deleted); */
+
+
+const position = document.querySelectorAll('.promo__menu-item');                            //постановка обработчика событий разным элементам
+position.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault;
+        e.target.style.color = 'red';
+    });
+});
